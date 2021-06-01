5c135f75dec1dc8945baf95665a9d0b13c7229e3
Sil Sanchez <ssanchez@makingsense.com>
2021-06-01 18:03:23 -0300

    Fix(UserNew): Rename page.

--- users-react-app/src/pages/NewUser.js
+++ users-react-app/src/pages/NewUser.js
@@ -3,45 +3,45 @@ import Card from '../components/Card/Card';
 import UserForm from '../components/UserForm/UserForm';
 
 
-class UsersNew extends React.Component {
+class NewUser extends React.Component {
     state = {
-        form : {
+        form: {
             name: '',
-            age:'',
-            rol:'',
-            email:''
+            age: '',
+            rol: '',
+            email: ''
         }
     };
-    
+
     handleChange = e => {
         this.setState({
-            form:{
+            form: {
                 ...this.state.form,
-                [e.target.name] : e.target.value,
+                [e.target.name]: e.target.value,
             }
         })
     }
     render() {
         return (
-            <>
+            <React.Fragment>
                 <div className="container dp-rowflex p-t-54 p-b-54">
                     <section className="col-sm-6">
                         <article>
                             <div className="dp-align-center">
-                                <Card nombre={this.state.form.name} edad={this.state.form.age} rol={this.state.form.rol} mail={this.state.form.email}/>
+                                <Card nombre={this.state.form.name} edad={this.state.form.age} rol={this.state.form.rol} mail={this.state.form.email} />
                             </div>
                         </article>
                     </section>
                     <section className="col-sm-6">
                         <article>
-                           <UserForm onChange={this.handleChange} formValues={this.state.form}/> 
+                            <UserForm onChange={this.handleChange} formValues={this.state.form} />
                         </article>
                     </section>
                 </div>
-            </>
+            </React.Fragment>
         );
     }
 
 }
 
-export default UsersNew;
+export default NewUser;
