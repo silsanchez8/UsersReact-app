a73b05b9225301693077102839be02f3037b8e88
Sil Sanchez <ssanchez@makingsense.com>
2021-06-04 13:46:05 -0300

    Fix(Layout): Use arrow function.

--- users-react-app/src/pages/Layout.js
+++ users-react-app/src/pages/Layout.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "../components/Header/Header";
 
-function Layout(props) {
+const Layout = (props) => {
   return (
     <>
       <Header />
       {props.children}
     </>
   );
-}
+};
 export default Layout;
